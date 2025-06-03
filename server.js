79e6af1ef3b5001b103b4a79c83cf4fde8c1f805
Anas10384 <factsmd881@gmail.com>
2025-06-03 18:17:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const channels = {
   '2': 'http://exm3u.extraott.com:80/live/2249871453/1991220000/401627870.m3u8',
   '3': 'http://exm3u.extraott.com:80/live/2249871453/1991220000/187263.m3u8',
   '4': 'http://ptu.ridsys.in/riptv/live/stream20/index.m3u8',
-  '5': 'https://demo.unified-streaming.com/k8s/features/stable/avc1_2500000.m3u8'
+  '5': 'http://exm3u.extraott.com:80/live/2249871453/1991220000/400157873.m3u8'
+  '6': 'http://exm3u.extraott.com:80/live/2249871453/1991220000/400157873.m3u8'
 };
 
 app.get('/box.ts', (req, res) => {
